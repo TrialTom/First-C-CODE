3fff7a3e7214efc066a5683d70d0bad38ef3dc27
TrialTom <108912578+TrialTom@users.noreply.github.com>
2022-07-19 17:59:30 +0800

    Add files via upload

---
+++ test-07-19/test.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+//判断一个数是否为奇数
+int main()
+{
+	double  a = 0;
+	int A = 0;
+	printf("输入一个整数：");
+	scanf("%lf", &a);
+	/*printf("%lf\n", a);*/
+	A = (int)a;
+	if (0 != a - A)
+	{
+		printf("输入的是小数，小数既不是奇数也不是偶数！");
+		return 0;
+	}
+	else
+	{
+		if (0 == A % 2 || 0 == A)
+		{
+			printf("偶数");
+			return 0;
+		}
+		else {
+			printf("奇数");
+			return 0;
+		}
+	}
+	return 0;
+}
